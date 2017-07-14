8b8dffaf924364ad9b925bb7e27d2cc73ed53db2
jhson <jhson@encar.com>
2017-07-14 18:33:33 +0900

    action type, creator, reducers 추가 및 수정

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import * as types from './ActionTypes';
 
+export const create = () => ({
+    type: types.CREATE
+});
+
+export const remove = () => ({
+    type: types.REMOVE
+});
+
 export const increment = () => ({
     type: types.INCREMENT
 });
